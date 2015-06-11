91a9c1692cafc2442481bd6a077f1ac8fbc99e21
Kyle Manna <kyle@kylemanna.com>
2015-06-11 14:33:38 -0700

    simple: Hack in simple code to test stuff

---
+++ libuavcan_drivers/linux/apps/simple/simple.hpp
@@ -0,0 +1,69 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2014, 2015 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/**
+ * @author Kyle Manna <kyle@kylemanna.com>
+ */
+
+#pragma once
+
+
+#include <uavcan/uavcan.hpp>
+#include <uavcan/equipment/ctrl/Command.hpp>
+
+class RingBuffer;
+
+class UavcanSimple : uavcan::Noncopyable
+{
+public:
+	static const char *const NAME;
+
+	UavcanSimple(uavcan::INode &node);
+
+	const char *get_name() const { return NAME; }
+
+	int init();
+
+private:
+	void simple_sub_cb(const uavcan::ReceivedDataStructure<uavcan::equipment::ctrl::Command> &msg);
+
+	typedef uavcan::MethodBinder < UavcanSimple *,
+		void (UavcanSimple::*)
+		(const uavcan::ReceivedDataStructure<uavcan::equipment::ctrl::Command> &) >
+		SimpleCbBinder;
+
+	uavcan::Subscriber<uavcan::equipment::ctrl::Command, SimpleCbBinder> _sub_ctrl_cmd;
+	uavcan::Publisher<uavcan::equipment::ctrl::Command>			         _pub_ctrl_cmd;
+
+	uint32_t value;
+};
